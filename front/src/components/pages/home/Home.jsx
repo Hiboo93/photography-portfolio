1a4582dda4d6319e1e2f57613962044817447b66
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-12-17 22:28:16 +0100

    🚧 F06/page: setting up animation in the home component

--- front/src/components/pages/home/Home.jsx
+++ front/src/components/pages/home/Home.jsx
@@ -21,7 +21,13 @@ function Home() {
         {/* text & img wrapper */}
         <div className="flex flex-col justify-center">
           {/* text */}
-          <div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
+          <motion.div
+            initial={{ opacity: 0, y: "-50%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-50%" }}
+            transition={transition1}
+            className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start"
+          >
             <h1 className="h1">
               photographer <br /> & film maker
             </h1>
@@ -33,12 +39,18 @@ function Home() {
                 Hire me
               </Link>
             </ButtonRelume>
-          </div>
+          </motion.div>
           {/*  image  */}
           <div className="flex justify-end max-h-96 lg:max-h-max">
-            <div className="relative lg:-right-40 overflow-hidden">
-              <img src={WomanImg} alt="women" />
-            </div>
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0 }}
+              transition={transition1}
+              className="relative lg:-right-40 overflow-hidden"
+            >
+              <motion.img whileHover={{scale: 1.1}} transition={transition1} src={WomanImg} alt="women" />
+            </motion.div>
           </div>
         </div>
       </div>
