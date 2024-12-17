6eacadd17ab2d9b7fb2cbf0873d55ff7a99eed2c
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-12-17 22:09:03 +0100

    🚧 F05/page: contact component layout work and animation setup

--- front/src/components/common/design-system/AnimeRoutes.jsx
+++ front/src/components/common/design-system/AnimeRoutes.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import Home from '../../pages/home/Home';
-import About from '../../pages/about/About';
-import Portfolio from '../../pages/portfolio/Portfolio';
-import Contact from '../../pages/contact/Contact';
+import Home from "../../pages/home/Home";
+import About from "../../pages/about/About";
+import Portfolio from "../../pages/portfolio/Portfolio";
+import Contact from "../../pages/contact/Contact";
+import { AnimatePresence } from "motion/react";
 
 const AnimeRoutes = () => {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/about' element={<About />}/>
-      <Route path='/portfolio' element={<Portfolio />}/>
-      <Route path='/contact' element={<Contact />}/>
-    </Routes>
-  )
-}
+    <AnimatePresence initial={true} mode="wait">
+      <Routes key={location.pathname} location={location}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
-export default AnimeRoutes
+export default AnimeRoutes;
