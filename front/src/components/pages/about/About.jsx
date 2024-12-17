3e8d77e0964d360b7c64fb069ea992c44b842e6a
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-12-17 22:54:19 +0100

    🚧 F06/page: animation setup in home and portfolio component

--- front/src/components/pages/about/About.jsx
+++ front/src/components/pages/about/About.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+
+import { motion } from "motion/react";
+import { transition1 } from "../../transition";
+
 import { Link } from "react-router-dom";
 
-import ButtonRelume from "../../common/design-system/Button/ButtonRelume.jsx"
+import ButtonRelume from "../../common/design-system/Button/ButtonRelume.jsx";
 
 import WomanImg from "../../../assets/img/about/woman.png";
 
 function About() {
   return (
-    <section className="section ">
+    <motion.section
+      initial={{ opacity: 0, y: "100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "100%" }}
+      transition={transition1}
+      className="section"
+    >
       <div className="container mx-auto h-full relative">
         {/* text & img wrapper */}
         <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
@@ -16,7 +26,13 @@ function About() {
             <img src={WomanImg} alt="woman" />
           </div>
           {/* text */}
-          <div className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start">
+          <motion.div
+            initial={{ opacity: 0, y: "-80%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-80%" }}
+            transition={transition1}
+            className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start"
+          >
             <h1 className="h1">About Me</h1>
             <p className="mb-12 max-w-sm">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.{" "}
@@ -32,10 +48,10 @@ function About() {
             <ButtonRelume>
               <Link to={"/portfolio"}>VIEW MY WORK</Link>
             </ButtonRelume>
-          </div>
+          </motion.div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 }
 
